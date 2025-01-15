86e1205e20b6b275131b955716c6910ef981bde5
youngah kim <duddk9804@naver.com>
2025-01-15 23:20:01 +0900

    0115
    주석삭제

--- javascript/menuscroll.js
+++ javascript/menuscroll.js
@@ -1,11 +1,11 @@
 $(window).scroll(() => {
-    let ws = $(this).scrollTop(); // 현재 스크롤 위치값
-    let fixWrap = $('.website-list').offset().top; // 고정박스 위치
-    let move = ws - fixWrap; // 이동하는 구간
-    let end = $('.lists').width() - $(window).innerWidth(); // 끝지점
+    let ws = $(this).scrollTop();
+    let fixWrap = $('.website-list').offset().top;
+    let move = ws - fixWrap;
+    let end = $('.lists').width() - $(window).innerWidth();
 
-    // 이미지 회전을 위한 각도 계산
-    let rotation = (move % 360); 
+
+    let rotation = (move % 360);
 
     if (ws > fixWrap && ws < fixWrap + end) {
         $('.lists').css('transform', `translateX(-${move}px)`);
@@ -13,6 +13,6 @@ $(window).scroll(() => {
         $('.lists').css('transform', `translateX(-${end}px)`);
     }
 
-    // 이미지 회전
+
     $('.menubar .point').css('transform', `rotate(${rotation}deg)`);
 });
